90477f4d7e568a452cd2a8afce5174d71294496a
Camila <ccontreras20@stuy.edu>
2021-01-03 12:48:32 -0800

    Bubble sort worksgit add Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -5,18 +5,15 @@ public class Sorts {
   //Bubble Sort of int Arrays
   //Upon completion, elements of the array will be in increasing OrderedArrayList
   //@param data the elements to be sorted
-  public static void bubbleSort(int[]data) {
-    boolean sort = false;
-    for (int end = 0; !sort; end++) {
-      sort= true;
-      for (int i =0; i< data.length-1-end;i++) {
-        if (data[i] > data[i+1]){
-          sort = false;
-          int notSort = data[i];
-          data[i] = data[i+1];
-          data[i+1] = notSort;
+  public static void bubbleSort(int[] data) {
+      for (int i =0; i< data.length-1;i++) {
+        for (int j = 0; j<data.length-i-1;j++) {
+          if (data[j] > data[j+1]){
+            int notSort = data[j];
+            data[j] = data[j+1];
+            data[j+1] = notSort;
+          }
         }
       }
     }
   }
-}
