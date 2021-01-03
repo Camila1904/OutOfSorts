d67c040149b326a69b0444ba9a79772df1216ac8
Camila <ccontreras20@stuy.edu>
2021-01-03 15:13:32 -0800

    insertionSort done, must be tested

--- Sorts.java
+++ Sorts.java
@@ -29,4 +29,15 @@ public class Sorts {
         data[s] = notSorted;
       }
     }
+    public static void insertionSort(int[] data) {
+      for (int i=0; i<data.length;i++) {
+        int j = i;
+        while ((j>0) && (data[j-1]>data[j])) {
+          int nonSort = data[j];
+          data[j]=data[j-1];
+          data[j-1] = nonSort;
+          j--;
+        }
+      }
+    }
   }
